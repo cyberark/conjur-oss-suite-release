2a04118cb35470f7fb9418269323ed7b8d4dfad0
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2020-02-05 15:39:25 +0000

    clean up

--- cmd/changelog-parser/main.go
+++ cmd/changelog-parser/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
 	"strings"
 
 	"gopkg.in/yaml.v3"
+
+	changelogPkg "github.com/cyberark/conjur-oss-suite-release/pkg/changelog"
 )
 
 type DescribedObject struct {
@@ -79,8 +79,6 @@ func latestVersionToExactVersion(provider string, repo string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	request.Header.Add("Authorization", "token 725ae82aaf466b77aa3e1260cc3f93e64e2d29dc")
 
 	log.Printf("  Fetching %s release info...", releaseUrl)
 	response, err := client.Do(request)
@@ -201,82 +199,17 @@ func main() {
 		res += "## " + url + "\n"
 		res += changelog + "\n"
 	}
-	err = ioutil.WriteFile("tmpCHANGELOG.md", []byte(res), 0644)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+
+	log.Println(res)
 
 	log.Printf("Changelog parser completed!")
-}
-
-// patterns
-var semverRgx = regexp.MustCompile(`\[?v?([\w\d.-]+\.[\w\d.-]+[a-zA-Z0-9])]?`)
-var dateRgx = regexp.MustCompile(`.*[ ](\d\d?\d?\d?[-/.]\d\d?[-/.]\d\d?\d?\d?).*`)
-
-func parseChangelog(changelog string) ([]*VersionChangelog, error) {
-	scanner := bufio.NewScanner(strings.NewReader(changelog))
-
-	var versionChangelog *VersionChangelog
-	var changeLogs []*VersionChangelog
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		// skip line if it's a link label
-		match, _ := regexp.MatchString(`^\[[^[\]]*\] *?:`, line)
-		if match {
-			continue
-		}
-
-		// new version found!
-		match, _ = regexp.MatchString(`^##? ?[^#]`, line)
-		if match {
-			if versionChangelog != nil && versionChangelog.Title != "" && versionChangelog.Version != "" {
-				changeLogs = append(changeLogs, versionChangelog)
-			}
-
-			versionChangelog = &VersionChangelog{}
-
-			versionChangelog.Title = string(line)[2:]
-
-			version := semverRgx.FindStringSubmatch(line)
-			if len(version) == 0 {
-				continue
-			}
-			versionChangelog.Version = string(version[1])
-
-			date := dateRgx.FindStringSubmatch(versionChangelog.Title)
-			if len(date) == 0 {
-				continue
-			}
-			versionChangelog.Date = string(date[1])
-
-			continue
-		}
-
-		// accumulate current changelog's body
-		if versionChangelog != nil {
-			versionChangelog.Body += fmt.Sprintln(line)
-		}
-	}
-	err := scanner.Err()
-
-	return changeLogs, err
-}
-
-type VersionChangelog struct {
-	Version string
-	Title string
-	Date string
-	Body string
 }
 
 func extractVersionChangeLog(
 	changelog string,
 	version string,
 ) (string, error) {
-	versionChangelogs, err := parseChangelog(changelog)
+	versionChangelogs, err := changelogPkg.Parse(changelog)
 	if err != nil {
 		return "", err
 	}
---
+++ pkg/changelog/parse.go
@@ -0,0 +1,119 @@
+package changelog
+
+import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type VersionChangelog struct {
+	Version string
+	Title string
+	Date string
+	Body string
+}
+
+// semantic versioning pattern
+// [a.b.c], e.g. 2.2.3-pre.1, 2.0.0-x.7.z.92, v1.3.0.
+const semverRgx = `\[?v?([\w\d.-]+\.[\w\d.-]+[a-zA-Z0-9])\]?`
+// date pattern.
+// YYYY-MM-DD (or DD.MM.YYYY, D/M/YY, etc.)
+const dateRgx = `.*[ ](\d\d?\d?\d?[-/.]\d\d?[-/.]\d\d?\d?\d?).*`
+// link label pattern
+// [a.b.c]: http://altavista.com
+const linkLabelRgx = `^\[[^[\]]*\] *?:`
+// version line pattern
+// ## x.y.z - YYYY-MM-DD (or DD.MM.YYYY, D/M/YY, etc.)
+const versionLineRgx = `^##? ?[^#]`
+
+// Parse extracts and returns a slice of changelogs, one for each version.
+// Parse assumes a changelog structured roughly like so:
+//
+// # changelog title
+//
+// A cool description (optional).
+//
+// ## unreleased
+// * foo
+//
+// ## x.y.z - YYYY-MM-DD (or DD.MM.YYYY, D/M/YY, etc.)
+// * bar
+//
+// ## [a.b.c]
+//
+// ### Changes
+//
+// * Update API
+// * Fix bug #1
+//
+// ## 2.2.3-pre.1 - 2013-02-14
+// * Update API
+//
+// ## 2.0.0-x.7.z.92 - 2013-02-14
+// * bark bark
+// * woof
+// * arf
+//
+// ## v1.3.0
+//
+// * make it so
+//
+// ## [1.2.3](link)
+// * init
+//
+// [a.b.c]: http://altavista.com
+func Parse(changelog string) ([]*VersionChangelog, error) {
+	scanner := bufio.NewScanner(strings.NewReader(changelog))
+
+	var versionChangelog *VersionChangelog
+	var changeLogs []*VersionChangelog
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// skip line if it's a link label!
+		if match, _ := regexp.MatchString(linkLabelRgx, line); match {
+			continue
+		}
+
+		// new version found!
+		if match, _ := regexp.MatchString(versionLineRgx, line); match {
+			hasPendingChangelog := versionChangelog != nil
+			hasPendingChangelog = hasPendingChangelog && versionChangelog.Title != ""
+			hasPendingChangelog = hasPendingChangelog && versionChangelog.Version != ""
+			if hasPendingChangelog {
+				changeLogs = append(changeLogs, versionChangelog)
+			}
+
+			versionChangelog = &VersionChangelog{}
+
+			// extract title
+			versionChangelog.Title = string(line)[2:]
+
+			// extract version
+			version := regexp.MustCompile(semverRgx).FindStringSubmatch(line)
+			if len(version) == 0 {
+				continue
+			}
+			versionChangelog.Version = string(version[1])
+
+			// extract date
+			date := regexp.MustCompile(dateRgx).FindStringSubmatch(line)
+			if len(date) == 0 {
+				continue
+			}
+			versionChangelog.Date = string(date[1])
+
+			continue
+		}
+
+		// accumulate pending changelog's body
+		if versionChangelog != nil {
+			versionChangelog.Body += fmt.Sprintln(line)
+		}
+	}
+	err := scanner.Err()
+
+	return changeLogs, err
+}
