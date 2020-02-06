99b349a1e4fbca6345a2d3a1a47252ba56acd181
Srdjan Grubor <sgnn7@sgnn7.org>
2020-02-06 12:04:30 -0600

    Remove unnecessary changelog size output
    This isn't needed anymore since it doesn't contain the real CHANGELOG
    content length.

--- cmd/changelog-parser/main.go
+++ cmd/changelog-parser/main.go
@@ -165,7 +165,6 @@ func collectChangelogs(repoConfig YamlRepoConfig) (
 			}
 
 			changelogs = append(changelogs, versionChangelog)
-			log.Printf("  Changelog size: %d", len(changelogs))
 		}
 	}
 	return changelogs, nil
