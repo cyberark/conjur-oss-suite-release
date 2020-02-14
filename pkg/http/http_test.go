73c52a45d1f12238148917b896295a9156e9a42a
Srdjan Grubor <sgnn7@sgnn7.org>
2020-02-14 09:29:54 -0600

    Added test for the http module
    This module had no testing and we're trying to keep our unit test
    coverage high so this should improve it a bit.

--- pkg/http/http_test.go
+++ pkg/http/http_test.go
@@ -1,9 +1,51 @@
 package http
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHttpClientGet(t *testing.T) {
-	t.Skip("TODO")
+	testPath := "/foo/bar"
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, req.URL.String(), testPath)
+		rw.Write([]byte("Page Content"))
+	}))
+	defer server.Close()
+
+	content, err := GetWithOptions(server.URL+testPath, server.Client())
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, []byte("Page Content"), content)
+}
+
+func TestHttpClientGetRequestUrlProblem(t *testing.T) {
+	_, err := Get("zzz")
+	if !assert.Error(t, err) {
+		return
+	}
+
+	assert.EqualError(t, err, "Get zzz: unsupported protocol scheme \"\"")
+}
+
+func TestHttpClientGetRequestBadStatusCode(t *testing.T) {
+	testPath := "/foo/bar"
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(404)
+	}))
+	defer server.Close()
+
+	_, err := GetWithOptions(server.URL+testPath, server.Client())
+	if !assert.Error(t, err) {
+		return
+	}
+
+	assert.EqualError(t, err, "code 404: "+server.URL+testPath+": ")
 }
