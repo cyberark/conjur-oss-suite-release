079a1ad82028e3c2b70d85954bc3086f678c2f4a
Srdjan Grubor <sgnn7@sgnn7.org>
2020-02-20 11:03:06 -0600

    Added integration tests for the main entrypoint
    These test will ensure that we have the exact output that we are
    expecting from the main program.

---
+++ cmd/changelog-parser/main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode.")
+	}
+
+	// Construct a path to our test repositories yaml
+	thisDir, err := os.Getwd()
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	testRepositoriesYml := filepath.Join(thisDir, "testdata", "repositories.yml")
+
+	// We have to run from toplevel dir to be able to use the defaults
+	os.Chdir("../..")
+
+	for _, tt := range []string{"changelog", "release"} {
+		t.Run(tt, func(t *testing.T) {
+			// Create a tempdir to write the out output to
+			outputDir, err := ioutil.TempDir("", "main_test")
+			if !assert.NoError(t, err) {
+				return
+			}
+			defer os.RemoveAll(outputDir)
+
+			outputFile := filepath.Join(outputDir, tt+"_output.txt")
+			outputDate, _ := time.Parse(time.RFC3339, "2020-02-19T12:00:00Z")
+
+			// Run the test
+			runParser(cliOptions{
+				Date:               outputDate,
+				OutputFilename:     outputFile,
+				OutputType:         tt,
+				RepositoryFilename: testRepositoriesYml,
+				Version:            "Unreleased",
+			})
+
+			log.Print("Verifying test result...")
+
+			outputFileContent, err := ioutil.ReadFile(outputFile)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			// Tests are expected at "./testdata/expected_<type>_output.txt"
+			expectedOutputFile := filepath.Join(thisDir, "testdata", "expected_"+tt+"_output.txt")
+			expectedOutput, err := ioutil.ReadFile(expectedOutputFile)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			assert.Equal(t, string(expectedOutput), string(outputFileContent))
+		})
+	}
+}
